1f5f251fa20c7efd764eafc4bb0532d9acda8766
zemundo <36026114+zemundo@users.noreply.github.com>
2018-02-01 01:01:55 -0500

    Update cryptoPricesV2.py

--- cryptoPricesV2.py
+++ cryptoPricesV2.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import datetime
 
-#now = datetime.datetime.now() Uzima danasnji datum i lokalno vreme
+
 filename = "Crypto Data " + datetime.datetime.now().strftime("%Y-%m-%d")
 filenameStr = str(filename)
 
@@ -35,38 +35,23 @@ def findCryptoData():
 		coin24hChange.append(change.text.replace('\n', '' ))	
 
 
-'''def printNames():
-	
-
-	nl = '\n'
-	for a,b,c,d in zip(coinName,coinPrice,coinMarketCap,coin24hChange):
-		print('\n''Coin:' + " "+ str(a) + '\n''Price:' + " "+ str(b)+ '\n''Market Cap:' + " "+ str(c) + '\n''24h Change:' + " "+ str(d))
-'''
 
 
-#def dateAndTime(): //Ispis Datum Vreme
-	#print(now)
 
 def writeToFile():
 	filename = filenameStr +'.csv'
 	f = open(filename, "w")
 	headers = "Coin Name, Coin Price, Coin Market Cap, Coin Change 24h \n"
 	f.write(headers)
-	'''
-	cName = ("\n".join(coinName))
-	cPrice = ("\n".join(coinPrice))
-
-	f.write("\n".join(coinName) + "\n".join(coinPrice))
-	'''
+	
 	for i,o,p,q in zip(coinName,coinPrice,coinMarketCap,coin24hChange):
 		f.write(i+ "," + o + "," + p + "," +q + "\n")
-	#f.write(coinName + "," + coinPrice+ "\n")
+	
 	f.close()
 
 
 
 findCryptoData()
 writeToFile()
-#dateAndTime() //Ispis Datum Vreme
 
-#printNames()
+
